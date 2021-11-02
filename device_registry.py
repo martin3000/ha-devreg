2031c02044c1e3c051bce834e167cc354c76df26
Martin <github@mx.schlatter.eu>
2021-11-02 12:18:30 +0100

    Update device_registry.py

--- device_registry.py
+++ device_registry.py
@@ -1,6 +1,7 @@
 import json
 import csv
 
+# read areas/rooms
 with open('.storage/core.area_registry') as json_file:
     areareg = json.load(json_file)
 
@@ -10,19 +11,13 @@ for area in areareg["data"]["areas"]:
     name= area["name"]
     areas[id] = name
 
+# read device registry
 with open('.storage/core.device_registry') as json_file:
     d = json.load(json_file)
-
-#>>> data["data"]["devices"][20]
-#{'config_entries': ['d6a5c08bfbec4a1eba04d675f19cdf59'], 'connections': [['zigbee', '00:15:8d:00:04:14:05:27']], 'identifiers': [['zha', '00:15:8d:00:04:14:05:27']], 
-#  'manufacturer': 'LUMI', 'model': 'lumi.sensor_motion.aq2', 'name': 'LUMI lumi.sensor_motion.aq2', 
-# 'sw_version': None, 'entry_type': None, 'id': '6cce8aa3217f4e1688df18cdaf0d6a4d', 'via_device_id': '283c921e0f6742418aa167e4ee0fe7db', 
-# 'area_id': '77475f19639f41738e46bf768b44c053', 'name_by_user': 'LUMI lumi.sensor_motion.aq2_Kuechenarbeitsplatte', 'disabled_by': None, 'configuration_url': None}
 
 with open('devreg.csv', mode='w') as devreg:
  fieldnames = ["name","friendlyname","manufacturer","model","conn","id domain","device id","ha-id","area"]
  writer = csv.writer(devreg, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)  # csv.QUOTE_MINIMAL
- #writer.writeheader()
  writer.writerow(fieldnames)
  for device in d["data"]["devices"]:
     conn = device["connections"][0][0] if len(device["connections"])>0 else None
@@ -36,7 +31,5 @@ with open('devreg.csv', mode='w') as devreg:
     internal_id = device["id"]
     if area in areas:
         area = areas[area]  # replace id with name
-    #print(model)
 
     writer.writerow([name,friendlyname,manu,model,conn,iddomain,id,internal_id,area])
-    #print(name,conn,ident,manu,model,name)
